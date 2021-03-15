740d80cb3e8d43ef285fe800cd3421fc9e861b76
redrexaye <rizki.14117084@student.itera.ac.id>
2021-03-15 23:30:47 +0700

    Customer Fix All

--- application/views/Home/customer.php
+++ application/views/Home/customer.php
@@ -40,7 +40,7 @@
 
 
 <!-- pop up add customer -->
-<div class="modal fade  px-5 " id="popup-customer" tabindex="-1" role="dialog" aria-labelledby="modal-block-large" aria-hidden="true">
+<div class="modal fade mt-5" id="popup-customer" tabindex="-1" role="dialog" aria-labelledby="modal-block-large" aria-hidden="true">
     <div class="modal-dialog modal-md" role="document">
         <div class="modal-content">
             <div class="modal-header bg-primary-dark">
@@ -117,7 +117,7 @@
 
 
 <!-- pop up update customer -->
-<div class="modal fade  px-5 " id="popup-update-customer" tabindex="-1" role="dialog" aria-labelledby="modal-block-large" aria-hidden="true">
+<div class="modal fade mt-5 " id="popup-update-customer" tabindex="-1" role="dialog" aria-labelledby="modal-block-large" aria-hidden="true">
     <div class="modal-dialog modal-md" role="document">
         <div class="modal-content">
             <div class="modal-header bg-primary-dark">
@@ -192,7 +192,7 @@
 <!-- end pop up update customer -->
 
 <!-- pop up detail customer -->
-<div class="modal fade  " id="popup-detail-customer" tabindex="-1" role="dialog" aria-labelledby="modal-block-large" aria-hidden="true">
+<div class="modal fade mt-5 " id="popup-detail-customer" tabindex="-1" role="dialog" aria-labelledby="modal-block-large" aria-hidden="true">
     <div class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
             <div class="modal-header bg-primary-dark">

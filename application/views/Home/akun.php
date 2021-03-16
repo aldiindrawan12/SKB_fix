7f553f78f8638cc29cc9c426a4abaa9892866732
redrexaye <rizki.14117084@student.itera.ac.id>
2021-03-16 17:37:11 +0700

    Update akun.php

--- application/views/Home/akun.php
+++ application/views/Home/akun.php
@@ -84,7 +84,7 @@
 </div>
 <!-- end pop up tambah akun -->
 <!-- pop up update akun -->
-<div class="modal fade" id="popup-update-akun" tabindex="-1" role="dialog" aria-labelledby="modal-block-large" aria-hidden="true">
+<div class="modal fade mt-3" id="popup-update-akun" tabindex="-1" role="dialog" aria-labelledby="modal-block-large" aria-hidden="true">
     <div class="modal-dialog modal-md" role="document">
         <div class="modal-content">
             <div class="modal-header bg-primary-dark">

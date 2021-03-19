1dcde6417ac6f52d46d9f61c928b3fdc2e57ba7a
aldi indrawan <indrawanaldi75@gmail.com>
2021-03-19 12:01:36 +0700

    dasd

--- application/controllers/Detail.php
+++ application/controllers/Detail.php
@@ -24,7 +24,7 @@ class Detail extends CI_Controller {
             $data["customer"] = $this->model_home->getcustomerbyid($data["jo"]["customer_id"]);
             $data["mobil"] = $this->model_home->getmobilbyid($data["jo"]["mobil_no"]);
             $data["supir"] = $this->model_home->getsupirbyid($data["jo"]["supir_id"]);
-            $data["satuan"] = $this->model_home->getallsatuan();
+            // $data["satuan"] = $this->model_home->getallsatuan();
             if($asal=="JO"){
                 $data["page"] = "JO_page";
                 $data["collapse_group"] = "Perintah_Kerja";

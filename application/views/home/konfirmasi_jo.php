aaf9eb18af6a4bfa222604d5fcee8cdfa7e97681
redrexaye <rizki.14117084@student.itera.ac.id>
2021-03-28 20:48:36 +0700

    update

--- application/views/home/konfirmasi_jo.php
+++ application/views/home/konfirmasi_jo.php
@@ -4,7 +4,7 @@
     </div> 
     <div class="card shadow mb-4">
     <div class="card-header py-3">
-        <h6 class="m-0 font-weight-bold text-primary">Job Order(JO)</h6>
+        <h6 class="m-0 font-weight-bold text-primary">Dalam Perjalanan</h6>
     </div>
     <!-- tabel konfirmasi JO -->
     <div class="card-body">

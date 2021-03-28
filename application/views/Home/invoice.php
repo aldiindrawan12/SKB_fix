889ee89546fc7d68b4e8aac512831007f6a8e1f4
redrexaye <rizki.14117084@student.itera.ac.id>
2021-03-28 20:49:08 +0700

    Update invoice.php

--- application/views/Home/invoice.php
+++ application/views/Home/invoice.php
@@ -1,6 +1,6 @@
 <div class="container">
     <div class="d-sm-flex align-items-center justify-content-between mb-4">
-        <h1 class="h3 mb-0 text-gray-800 mt-3 mb-3">Tambah Invoice</h1>
+        <h1 class="h3 mb-0 text-gray-800 mt-3 mb-3">Buat Invoice</h1>
     </div> 
     <div class="card shadow">
     <!-- form invoice -->

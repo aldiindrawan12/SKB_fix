aeebdb707b541258e5b7309559274350bdf44c2c
redrexaye <rizki.14117084@student.itera.ac.id>
2021-03-28 21:57:24 +0700

    update all buat invoice

--- application/views/Home/invoice.php
+++ application/views/Home/invoice.php
@@ -1,6 +1,6 @@
 <div class="container">
-    <div class="d-sm-flex align-items-center justify-content-between mb-4">
-        <h1 class="h3 mb-0 text-gray-800 mt-3 mb-3">Buat Invoice</h1>
+    <div class="d-sm-flex align-items-center justify-content-between mb-3">
+        <h1 class="h4 mb-0 text-gray-800  ">Buat Invoice</h1>
     </div> 
     
 
@@ -9,11 +9,11 @@
         <div class="card-body">
         <form action="<?=base_url("index.php/form/insert_invoice")?>" method="POST">
             <div class="row">
-                <div class="col-md-5 border rounded border-secondary">
-                    <div class="form-group">
+                <div class="col-md-4 border rounded mr-4 ml-3">
+                    <div class="form-group mt-3">
                         <label class="form-label font-weight-bold " for="customer_id">Customer</label>
-                        <select name="customer_id" id="customer_id" class="form-control selectpicker" data-live-search="true" required onchange="customer()">
-                            <option class="font-w700" disabled="disabled" selected value="">Customer</option>
+                        <select name="customer_id" id="customer_id" class="form-control selectpicker col-md-10" data-live-search="true" required onchange="customer()">
+                            <option class="font-w700 " disabled="disabled" selected value="">Customer</option>
                             <?php foreach($customer as $value){?>
                                 <option value="<?=$value["customer_id"]?>"><?=$value["customer_name"]?></option>
                             <?php } ?>
@@ -21,7 +21,7 @@
                     </div>
                     <div class="form-group">
                         <label class="form-label font-weight-bold" for="invoice_ppn">PPN</label>
-                        <select name="invoice_ppn" id="invoice_ppn" class="form-control" required>
+                        <select name="invoice_ppn" id="invoice_ppn" class="form-control col-md-10" required>
                             <option class="font-w700" disabled="disabled" selected value="">PPN</option>
                             <option class="font-w700" value="Ya">Ya</option>
                             <option class="font-w700" value="Tidak">Tidak</option>
@@ -30,22 +30,19 @@
                     <div class="form-group">
                         <label for="invoice_id" class="form-label font-weight-bold">Invoice Kode</label>
                         <div class="row">
-                            <input autocomplete="off" type="text" class="form-control col-md-3" id="invoice_id1" name="invoice_id1" required>
-                            <input autocomplete="off" type="text" class="form-control col-md-6" id="invoice_id2" name="invoice_id2" required readonly>
+                            <input autocomplete="off" type="text" class="form-control col-md-2 ml-3 mr-1" id="invoice_id1" name="invoice_id1" required>
+                            <input autocomplete="off" type="text" class="form-control col-md-4  mr-1" id="invoice_id2" name="invoice_id2" required readonly>
                             <input autocomplete="off" type="text" class="form-control col-md-3" id="invoice_id3" name="invoice_id3" required readonly>
                         </div>
                     </div>
                     <div class="form-group">
                         <label for="invoice_tgl" class="form-label font-weight-bold">Tgl.Invoice</label>
-                        <input autocomplete="off" type="text" class="form-control" id="invoice_tgl" name="invoice_tgl" required>
+                        <input autocomplete="off" type="text" class="form-control col-md-10" id="invoice_tgl" name="invoice_tgl" required>
                     </div>                    
                 </div>
-                <div class="col-md-4 border rounded border-secondary">
-                    <div class="form-group">
-                        <label for="invoice_keterangan" class="form-label font-weight-bold">Keterangan</label>
-                        <textarea class="form-control" name="invoice_keterangan" id="invoice_keterangan" rows="1" required></textarea>
-                    </div>
-                    <div class="form-group row">
+                <div class="col-md-4 border rounded mr-4">
+                         
+                    <div class="form-group row mt-3">
                         <label for="invoice_tonase" class="col-form-label col-sm-5 font-weight-bold">Total Tonase</label>
                         <div class="col-sm-7">
                             <input autocomplete="off" type="text" class="form-control" id="invoice_tonase" name="invoice_tonase" required readonly>
@@ -69,17 +66,24 @@
                             <input autocomplete="off" type="text" class="form-control" id="invoice_grand_total" name="invoice_grand_total" required readonly>
                         </div>
                     </div>
-                </div>
-                <div class="col border rounded border-secondary">
                     <div class="form-group">
                         <label class="form-label font-weight-bold " for="invoice_payment">Payment (hari)</label>
                         <input autocomplete="off" type="text" class="form-control" id="invoice_payment" name="invoice_payment" required>
                     </div>
                     <input type="text" id="data_jo" name="data_jo" required hidden>
                 </div>
+                <div class="col-md-3 border rounded mr-2">
+
+                <div class="form-group mt-3">
+                        <label for="invoice_keterangan" class="form-label font-weight-bold">Keterangan</label>
+                        <textarea class="form-control" name="invoice_keterangan" id="invoice_keterangan" rows="11" required></textarea>
+                    </div>
+
+
+                </div>
             </div>
-            <div class="col-md-12">
-                <button type="submit" class="btn btn-success ml-3 mt-5 float-md-right">Simpan</button>
+            <div class="col">
+                <button type="submit" class="btn btn-success float-right mt-3">Simpan</button>
             </div>
         </form>
     
@@ -89,7 +93,7 @@
 </div>
 
                 <!-- table invoice -->
-                <div class="card shadow">
+                <div class="card shadow mb-5">
                     <div class="card-body">
                         <div class="table-responsive">
                             <table class="table table-bordered" id="pilih-jo" width="100%" cellspacing="0">

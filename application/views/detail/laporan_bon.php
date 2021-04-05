b913fb10f94ef8120b9d95bb9dc5503ef55dc310
redrexaye <rizki.14117084@student.itera.ac.id>
2021-04-06 01:47:02 +0700

    Update laporan_bon.php

--- application/views/detail/laporan_bon.php
+++ application/views/detail/laporan_bon.php
@@ -5,18 +5,18 @@
     <div class="card-body">
         <!-- tampilan detail transaksi bon -->
         <div class="container" id="detail-bon_supir">
-            <table class="table table-bordered">
+            <table class="table table-bordered ">
                 <thead>
                     <tr>
-                        <th colspan=6 class="text-center"><?=$supir?></th>
+                        <th colspan=6 class="text-center font-weight-bold h5"><?=$supir?></th>
                     </tr>
                     <tr>
-                        <th>No.</th>
-                        <th>Tanggal Transaksi</th>
-                        <th>Keterangan</th>
-                        <th>Bayar</th>
-                        <th>Bon</th>
-                        <th>Sisa Bon</th>
+                        <th class="text-center">No.</th>
+                        <th class="text-center">Tanggal Transaksi</th>
+                        <th class="text-center">Keterangan</th>
+                        <th class="text-center">Bayar</th>
+                        <th class="text-center">Bon</th>
+                        <th class="text-center">Sisa Bon</th>
                     </tr>    
                 </thead>
                 <tbody>  
@@ -24,22 +24,22 @@
                             $sisa_bon=0;
                     foreach($transaksi_bon as $value){?>
                         <tr>
-                            <td><?= $n?></td>
-                            <td><?= $value["bon_tanggal"]?></td>
-                            <td><?= $value["bon_keterangan"]?></td>
+                            <td class="text-center"><?= $n?></td>
+                            <td class="text-center"><?= $value["bon_tanggal"]?></td>
+                            <td class="text-center"><?= $value["bon_keterangan"]?></td>
                             <?php if($value["bon_jenis"]=="Pembayaran"){
                                 $sisa_bon-=$value["bon_nominal"];?>
-                                <td>Rp.<?= number_format($value["bon_nominal"],2,',','.')?></td>
-                                <td>Rp.0</td>
+                                <td class="text-center">Rp.<?= number_format($value["bon_nominal"],2,',','.')?></td>
+                                <td class="text-center">Rp.0</td>
                             <?php }else{
                                 $sisa_bon+=$value["bon_nominal"];?>
-                                <td>Rp.0</td>
-                                <td>Rp.<?= number_format($value["bon_nominal"],2,',','.')?></td>
+                                <td class="text-center">Rp.0</td>
+                                <td class="text-center">Rp.<?= number_format($value["bon_nominal"],2,',','.')?></td>
                             <?php }
                             if($sisa_bon==0){?>
-                                <td>Lunas</td>
+                                <td class="text-center">Lunas</td>
                             <?php }else{?>
-                                <td>Rp.<?= number_format($sisa_bon,2,',','.')?></td>
+                                <td class="text-center">Rp.<?= number_format($sisa_bon,2,',','.')?></td>
                             <?php }?>
                         </tr>
                     <?php $n++;
@@ -49,4 +49,5 @@
         </div>
         <!-- end tampilan detail transaksi bon -->
     </div>
+</div>
 </div>

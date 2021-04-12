993c67e6ddca0393e894cba4a738f28fb8887bff
redrexaye <rizki.14117084@student.itera.ac.id>
2021-04-12 08:45:46 +0700

    Update paketan.php

--- application/views/home/paketan.php
+++ application/views/home/paketan.php
@@ -20,14 +20,14 @@
             <table class="table table-bordered" id="Table-Paketan" width="100%" cellspacing="0">
                 <thead>
                     <tr>
-                        <th class="text-center" scope="col">No</th>
-                        <th class="text-center" scope="col">Nama Customer</th>
-                        <th class="text-center" scope="col">Rute</th>
-                        <th class="text-center" scope="col">Jenis Mobil</th>
-                        <th class="text-center" scope="col">Ritase/Tonase</th>
-                        <th class="text-center" scope="col">Uang Jalan</th>
-                        <th class="text-center" scope="col">Inv./Tagihan</th>
-                        <th class="text-center" width="20%" scope="col">Aksi</th>
+                        <th class="text-center" width="5%" scope="col">No</th>
+                        <th class="text-center" width="20%" scope="col">Nama Customer</th>
+                        <th class="text-center" width="5%" scope="col">Rute</th>
+                        <th class="text-center" width="15%" scope="col">Jenis Mobil</th>
+                        <th class="text-center" width="12%" scope="col">Ritase/Tonase</th>
+                        <th class="text-center" width="15%" scope="col">Uang Jalan</th>
+                        <th class="text-center" width="15%" scope="col">Inv./Tagihan</th>
+                        <th class="text-center" width="12%" scope="col">Aksi</th>
                     </tr>
                 </thead>
                 <tbody>

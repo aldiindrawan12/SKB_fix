207c4224bfbb06c916c48b8fb5467c163929b457
redrexaye <rizki.14117084@student.itera.ac.id>
2021-04-12 08:19:38 +0700

    Update laporan_bon.php

--- application/views/detail/laporan_bon.php
+++ application/views/detail/laporan_bon.php
@@ -1,7 +1,7 @@
 <div class="card shadow mb-4 ml-5 mr-5 py-2 px-2">
     <div class="card-header py-3 d-sm-flex align-items-center justify-content-between mb-4">
-        <h6 class="m-0 font-weight-bold text-primary">Detail Bon Supir</h6>
-        <a href="" class="btn btn-primary btn-icon-split" onclick="print_bon()">
+        <h6 class="m-0 font-weight-bold text-primary ml-4">Detail Bon Supir</h6>
+        <a href="" class="btn btn-sm btn-primary btn-icon-split mr-3" onclick="print_bon()">
                 <span class="icon text-white-100">
                     <i class="fas fa-print"></i> 
                 </span>

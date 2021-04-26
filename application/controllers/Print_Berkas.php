5d465624103deb7f1597b14199c6c5c40fda1d33
aldi indrawan <indrawanaldi75@gmail.com>
2021-04-26 10:31:12 +0700

    Update Print_Berkas.php

--- application/controllers/Print_Berkas.php
+++ application/controllers/Print_Berkas.php
@@ -225,6 +225,13 @@ class Print_Berkas extends CI_Controller {
                 redirect(base_url());
             }
 			$data["data"] = $this->model_home->getjobyid($jo_id);
+			if($data["data"]["paketan_id"]==0){
+				$data["tipe_jo"] = "reguler";
+				$data["kosongan"] = $this->model_detail->getkosonganbyid($data["data"]["kosongan_id"]);
+			}else{
+				$data["paketan"] = $this->model_form->getpaketanbyid($data["data"]["paketan_id"]);
+				$data["tipe_jo"] = "paketan";
+			}
 			$data["jo_id"] = $data["data"]["Jo_id"];
 			$data["asal"] = "detail";
             $data["supir"] = $this->model_home->getsupirbyid($data["data"]["supir_id"]);
